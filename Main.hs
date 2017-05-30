6e4643eb5d41e2b04f8500893498daefec53e835
Dave Compton <davecompton7@gmail.com>
2017-05-30 00:26:41 +0000

    formatting changes

--- Main.hs
+++ Main.hs
@@ -6,6 +6,8 @@ prependMaybe :: Maybe a -> Maybe [a] -> Maybe [a]
 prependMaybe (Just v) (Just vs) = Just (v:vs)
 prependMaybe _ _ = Nothing
 
+-- convert a list of Maybe to a Maybe list.
+-- result is Nothing if any of values from the original list are Nothing
 allMaybe :: [Maybe a] -> Maybe [a]
 allMaybe = foldr prependMaybe (Just []) 
 
@@ -17,10 +19,12 @@ valid :: [Int] -> Bool
 valid ns = 
     let -- multiply first 8 values with even index by 2
         s0 = zipWith (\i n -> ((if odd i then 1 else 2) * n)) [1..] $ take 8 ns
+
         -- apply div/mod formula from site and sum up results
         s1 = sum $ fmap (\s -> ( s `div` 10 ) + s `mod` 10) s0
-        -- apply mod/mod formula from site and compare to 9th value ( 0 index ) 
-    in ns!!8 == (10 - (s1 `mod` 10)) `mod` 10
+
+    in  -- apply mod/mod formula from site and compare to 9th value ( 0 index ) 
+        ns!!8 == (10 - (s1 `mod` 10)) `mod` 10
 
 -- check a String to see if it represents a valid CUSIP
 checkCUSIP :: String -> Result
